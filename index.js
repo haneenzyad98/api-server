4a97a671c77fc8c4232d1ce83306b16244442103
Haneen zyad odebat <haneenzyad98@gmail.com>
2021-05-15 23:10:35 +0300

    lab4

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 'use strict';
 
 require('dotenv').config();
-const server = require('./src/server.js');
+const mongoose= require('mongoose')
+const server = require('./ src/server');
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+})
+.then(()=> console.log('conntected to mongoDB'))
+.catch((err)=> console.log(err));
+
+
 
 // start server
 server.listen(3000);
